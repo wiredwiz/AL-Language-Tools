67730bed36b2050dff0bd8e24528de6e27862023
Thaddeus Ryker <thad@edgerunner.org>
2025-07-27 03:54:06 -0400

    Added more to the driver program

--- ANTLR AL Parser Driver/Program.cs
+++ ANTLR AL Parser Driver/Program.cs
@@ -4,19 +4,23 @@ using Org.Edgerunner.Language.AL.Parsing.Preprocessing;
 
 namespace ANTLR_AL_Parser_Driver
 {
-   internal class Program
+   internal static class Program
    {
+      private static Stopwatch Watch;
+
       static void Main(string[] args)
       {
          var parser = new ALParser(["CLEAN24"]);
          var source = new StreamReader("SalesHeader.Table.al");
 
+         parser.PreProcessingFinished += Parser_PreProcessingFinished;
+         parser.ParsingFinished += Parser_ParsingFinished;
+
          //// Initial parse to warm things up
          //parser.Parse(source);
 
-         var watch = Stopwatch.StartNew();
+         Watch = Stopwatch.StartNew();
          var result = parser.Parse(source);
-         watch.Stop();
          if (parser.Errors.Count > 0)
          {
             foreach (var message in parser.Errors)
@@ -24,7 +28,21 @@ namespace ANTLR_AL_Parser_Driver
          }
          else
             Console.WriteLine("No errors encountered.");
-         Console.WriteLine($"done parsing in {watch.ElapsedMilliseconds} milliseconds");
+         Console.WriteLine("Press any key to close");
+         Console.ReadKey();
+      }
+
+      private static void Parser_ParsingFinished(object? sender, ParseEventArgs e)
+      {
+         Watch.Stop();
+         Console.WriteLine($"Finished parsing in {Watch.ElapsedMilliseconds} milliseconds");
+      }
+
+      private static void Parser_PreProcessingFinished(object? sender, ParseEventArgs e)
+      {
+         Watch.Stop();
+         Console.WriteLine($"Finished pre-processing in {Watch.ElapsedMilliseconds} milliseconds");
+         Watch.Restart();
       }
    }
 }
